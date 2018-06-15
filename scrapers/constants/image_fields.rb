d0db38ed672ad77ed78b903c9975f69b7edcbae1
Thien Vo <vohuythien@utexas.edu>
2018-06-15 04:09:30 -0500

    Renamed rb files and dirs

---
+++ scrapers/constants/image_fields.rb
@@ -0,0 +1,10 @@
+module ImageFields
+    NAME = 'name'
+    CAT = 'category'
+    TYPE = 'type'
+    URL = 'url'
+    DESC = 'description'
+    SIZE = 'size'
+    HEROID = 'heroID'
+    MATID = 'materialID'
+end
